6cfeabfbe93319d2f7a1832a3c3518762af1fed1
Roman Shashin <propeller1992@gmail.com>
2025-06-20 09:35:03 +0300

    fix: image paths

--- src/components/Products/ProductGrid.jsx
+++ src/components/Products/ProductGrid.jsx
@@ -1,13 +1,65 @@
 import { ProductCard } from "./ProductCard/ProductCard";
-import products from "../../assets/images.json";
-// import img1 from "../../assets/images/gsm.jpeg";
-// import img2 from "../../assets/images/organosilicon_liquids.webp";
-// import img3 from "../../assets/images/paint_and_varnish_materials.jpg";
-// import img4 from "../../assets/images/glue.jpg";
-// import img5 from "../../assets/images/el_isolate.jfif";
-// import img6 from "../../assets/images/tools.jfif";
-// import img7 from "../../assets/images/RTI.webp";
-// import img8 from "../../assets/images/cloack.jpg";
+// import products from "../../../public/assets/images.json";
+import img1 from "../../assets/images/gsm.jpeg";
+import img2 from "../../assets/images/organosilicon_liquids.webp";
+import img3 from "../../assets/images/paint_and_varnish_materials.jpg";
+import img4 from "../../assets/images/glue.jpg";
+import img5 from "../../assets/images/el_isolate.jfif";
+import img6 from "../../assets/images/tools.jfif";
+import img7 from "../../assets/images/RTI.webp";
+import img8 from "../../assets/images/cloack.jpg";
+
+const products = [
+  {
+    id: 1,
+    header: "Горюче-смазочные материалы",
+    description: "Чтобы смазать и поджечь",
+    url: img1,
+  },
+  {
+    id: 2,
+    header: "Кремнийорганические жидкости",
+    description: "Разжижили кремний и органику",
+    url: img2,
+  },
+  {
+    id: 3,
+    header: "Лакокрасочные материалы",
+    description: "Полачим покрасим, будет не бита не крашена",
+    url: img3,
+  },
+  {
+    id: 4,
+    header: "Клеевые материалы",
+    description: "Осталось только где-то пакетик раздобыть",
+    url: img4,
+  },
+  {
+    id: 5,
+    header: "Электроизоляционные материалы",
+    description: "Заизолируйся, а то сгоришь на работе",
+    url: img5,
+  },
+  {
+    id: 6,
+    header: "Инструмент",
+    description: "Прикрути приколоти",
+    url: img6,
+  },
+  {
+    id: 7,
+    header: "Резинотехнические изделия",
+    description:
+      "Прокладки, трубки, патрубки, втулки, уплотнители. Женщины кончились 😔",
+    url: img7,
+  },
+  {
+    id: 8,
+    header: "Технические ткани",
+    description: "Мама сшила мне штаны из березовой коры",
+    url: img8,
+  },
+];
 
 export function ProductGrid() {
   // console.log(products);
